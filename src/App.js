70e4b1ec3c54dcfeef3f08d4b417d4cc87a25f3e
aangargiansah23 <aangargiansah91@gmail.com>
2022-10-08 19:17:04 +0700

    conditional rendering pada project react

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      namaResto : 'Codepolitan'
+      namaResto : 'Codepolitan',
+      menuMakanan : [
+        {nama : 'Ayam Bakar', harga : 25000, stock : 10},
+        {nama : 'Nasi Goreng', harga : 22000, stock : 0},
+      ]
     }
   }
 
@@ -21,11 +25,20 @@ class App extends Component {
 
   render () { 
     return (
-      <div style={{width: 600, textAlign:'center', padding: '10px', margin:'auto'}}>
+      <div className='App'>
         <h1>Cafe and Resto {this.state.namaResto}</h1>
         <button onClick={()=> this.handleGantiNama('Programmer')}>Ganti Nama Resto</button>
         <h2>Menu Makanan</h2>
-        <MenuMakanan namaMenu="Ayam Bakar" hargaMenu="25.000" />
+        <MenuMakanan
+          namaMenu={this.state.menuMakanan[0].nama}
+          hargaMenu={this.state.menuMakanan[0].harga}
+          stock={this.state.menuMakanan[0].stock}
+        />
+        <MenuMakanan
+          namaMenu={this.state.menuMakanan[1].nama}
+          hargaMenu={this.state.menuMakanan[1].harga}
+          stock={this.state.menuMakanan[1].stock}
+        />
         <h2>Menu Minuman</h2>
         <MenuMinuman namaMenu="Juice Melon" hargaMenu="15.000"/>
       </div>
--- src/Component/MenuMakanan/MenuMakanan.js
+++ src/Component/MenuMakanan/MenuMakanan.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 var MenuMakanan = (props) => {
-  return (
+
+  if (props.stock !== 0) {
+    return (
       <div style={{border: '1px solid black', width: 300, margin: 'auto'}}>
         <p>Nama Makanan : {props.namaMenu} </p>
         <p>Harga : {props.hargaMenu}</p>
+        <p>Stock : {props.stock}</p>
       </div>
   );
+  } else {
+    return null;
+  }
 };
 
 export default MenuMakanan;
