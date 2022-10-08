0a3b1536d1ec1b71ad3cd65807ddc6127d90ef7a
aangargiansah23 <aangargiansah91@gmail.com>
2022-10-08 16:42:52 +0700

    membuat component

---
+++ src/Component/MenuMakanan/MenuMakanan.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+var MenuMakanan = () => {
+  return (
+      <div style={{border: '1px solid black', width: 300, margin: 'auto'}}>
+        <p>Nama Makanan : Ayam Bakar </p>
+        <p>Harga : 25.000</p>
+      </div>
+  );
+};
+
+export default MenuMakanan;
---
+++ src/Component/MenuMinuman/MenuMinuman.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MenuMinuman extends Component {
+  render () {
+    return (
+      <div style={{border: '1px solid black', width: 300, margin: 'auto'}}>
+        <p>Nama Minuman : Juice Apel </p>
+        <p>Harga : 15.000</p>
+      </div>
+    );
+  };
+};
+
+export default MenuMinuman;
